9e8a1fd834a2f0ff7098c2d62b208c95f9179e3d
githubotoro <yupuday.git@gmail.com>
2022-11-28 22:32:24 +0530

    test-commit

--- pages/dnft/[userlink].js
+++ pages/dnft/[userlink].js
@@ -23,8 +23,8 @@ const DnftRender = () => {
 
 				<center>
 					<div className="p-4 -mt-48 items-center">
-						<div className="w-full h-38 bg-blue-500 rounded-lg drop-shadow-lg border-8 border-indigo-800">
-							<div className="p-4 text-lg font-bold text-white">
+						<div className="w-full h-fit bg-blue-500 rounded-lg drop-shadow-lg border-8 border-indigo-800">
+							<div className="p-4 text-md font-bold text-white">
 								Something is brewing here!
 								<br />
 								<a
